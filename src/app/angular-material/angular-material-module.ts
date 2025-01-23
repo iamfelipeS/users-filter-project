a5485ef635b0875ad15f4115236c4a9e65a85348
iamfelipeS <flipe.silva@terra.com.br>
2025-01-22 21:00:44 -0300

    add: filter component

--- src/app/angular-material/angular-material-module.ts
+++ src/app/angular-material/angular-material-module.ts
@@ -5,20 +5,33 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from "@angular/material/divider";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
     imports: [
         MatButtonModule,
         MatInputModule,
-        MatFormFieldModule,
         MatListModule,
         MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
     ],
     exports: [
         MatButtonModule,
         MatInputModule,
-        MatFormFieldModule,
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule,
     ]
 })
 export class AngularMaterialModule { }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material-module";
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
     declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent,
+    UsersListComponent
   ],
     imports: [
-        AngularMaterialModule
+        FormsModule,
+        AngularMaterialModule,
+        CommonModule,
     ],
     exports: [
-      UserDetailsComponent
+      UserDetailsComponent,
+      FilterComponent,
     ],
 })
 

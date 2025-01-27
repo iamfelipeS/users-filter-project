6958b41484da15674ba0db400b4c57354134b848
iamfelipeS <flipe.silva@terra.com.br>
2025-01-27 12:42:26 -0300

    add: status pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { PhonePipe } from "./phone.pipe";
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 
 @NgModule({
     declarations: [
         PhonePipe,
         AddressPipe,
+        StatusPipe,
     ],
     exports: [
         PhonePipe,
         AddressPipe,
+        StatusPipe,
     ]
 })
 export class PipesModule { }
